e1888705dc598c78d41f6af38bfb3e39e1ad152e
Thomaz Lucca <thomazlucca@gmail.com>
2025-04-15 18:03:24 -0300

    criando a camada Services

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const pessoas = require('./pessoasRoute.js');
+
+module.exports = app => {
+    app.use(
+        express.json(),
+        pessoas
+    );
+};
+
